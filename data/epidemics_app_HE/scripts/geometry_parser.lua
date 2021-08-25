ec5cd582ad73e771398f38010bf7424012fd0f96
HeinzHermann <marvin.glaser91@gmail.com>
2021-08-25 14:20:11 +0200

    ugx parser done

--- data/epidemics_app_HE/scripts/geometry_parser.lua
+++ data/epidemics_app_HE/scripts/geometry_parser.lua
@@ -7,15 +7,19 @@ Purpose: Parse Vetrex information from .ugx files to correlate vetex position an
 
 --[[ rewrite name list of grid subsets to search condition ]] 
 function prep_names(name_list)
-	local string_list={}
+	local string_list = {}
+	local magic_char = {"(", ")", ".", "%", "+", "-", "*", "?", "[", "^", "$"}
 	if next(name_list)==nil then
 		print("Error, empty name list provided")
 		return
 	end
 	for i=1, #name_list do
-		local string="name=".."\""..name_list[i].."\""
-		print(string) -- test print
-		string_list[i]=string
+		local target = "name=".."\""..name_list[i].."\""
+		-- escape magic characters for search
+		for j=1, #magic_char do
+			target = string.gsub(target, "%"..magic_char[j], "%%"..magic_char[j])
+		end
+		string_list[i] = target
 	end
 	return string_list
 
@@ -66,14 +70,73 @@ end
 
 --[[ parse vertex-subset association ]]
 -- work in progress
-function get_association(file, comment)
+function get_association(file, comment, targets)
 
-	for string in file:lines() do
+	local associations = {}
+	local parse_next = false
+	local target_number = 0
+
+	for line in file:lines() do
 		local comment_line = (string.find(line, comment)==1)
 		if not(comment_line) then 
-		--	print()
+
+			-- if line can contain target
+			if string.find(line, "<subset name=") then
+				-- loop through targets
+				for i=1, #targets do
+					-- if line contains a target --> parse next
+					if string.find(line, targets[i]) then
+						parse_next = true
+						target_number = i
+						break
+					end
+				end
+
+				
+			else
+				-- if target found and line contains vertices
+				if (parse_next and string.find(line, "<vertices>")) then
+					-- if target has been parsed before, return error 
+					if associations[target_number] ~= nil then
+						print("Error, target subset has been parsed before")
+					else
+					-- create new association entry
+					associations[target_number] = {}
+					-- parse all vertex positions
+					for vertex_pos in string.gmatch(line, "%d+") do
+						-- NOTE: +1 because lua counts from 1, but .ugx file starts counting at 0
+						table.insert(associations[target_number], tonumber(vertex_pos)+1) 
+					end
+					parse_next = false
+					end
+				end
+
+
+			end
 		end
 
+	end
+
+	-- verify that every target was parsed
+	local err = false
+	local err_list = {}
+	for i=1, #targets do
+		if associations[i]==nil then
+			err = true
+			table.insert(err_list, targets[i])
+		end
+	end
+
+	if err then
+		print("Error, one or more of the targets could not be parsed")
+		print("Unparsed targets are:")
+		for i=1, #err_list do
+			print(err_list[i])
+		end
+		return nil
+	else
+	print("returning associations")
+	return associations
 	end
 end
 
@@ -81,9 +144,8 @@ end
 --[[ parse .ugx file for coodinates and vertex-subset association ]]
 -- work in progress
 function parse_data(path, filename, filetype, comment, name_list)
-	local data={}
-	local target_strings=prep_names(name_list)
-	local parse=false
+
+	local target_strings = prep_names(name_list)
 
 	-- open file and verify success
 	file = io.open(path.."/"..filename.."."..filetype, "r")
@@ -93,23 +155,43 @@ function parse_data(path, filename, filetype, comment, name_list)
 	end
 
 	positions = get_positions(file, comment)	
-	for i=1, #positions do
-		print("["..positions[i][1]..","..positions[i][2]..","..positions[i][3].."]" )
+	associations = get_association(file, comment, target_strings)
+	
+	-- add subset association to position data
+	for i=1, #associations do
+		for j=1, #associations[i] do
+			print(associations[i][j])
+			table.insert(positions[associations[i][j]], i)
+		end
 	end
 
-	--get_association(file, comment, target_strings)
+	-- insert number of subsets at beginning of output
+	table.insert(positions, 1, #associations)
+
+
+	--[[
+	-- evaluate final output
+	print("[")
+	print(positions[1])
+	for i=2, #positions do
+		io.write("["..positions[i][2])
+		for j=2, #positions[i] do
+			io.write(","..positions[i][j])
+		end
+		print("]")
+	end
+	print("]")
+	]]
 	
-	file:close()
-
-	-- testing
-	--print(path.."/"..filename.."."..filetype)
-
-
+	return positions
 end
 
 
 function testing()
 	-- testing grounds
+	print("===============================================")
+	print("Testing:")
+	--[[
 	print(string.find("xx#test", "#"))
 	if next({})==nil then
 		print("works as bool")
@@ -123,16 +205,50 @@ function testing()
 	print(next({4,5,6}, 2))
 	print(next({4,5,6}, 3))
 
-
-	print("=================")
 	print(string.match("0</vertex>", "-?%d+[^\"<]?%.?%d*"))
 
+	local table={}
+	table[2] = 4
+	print(table[2])
+	print(table[1])
+	table[1] = 3
+	print(table[1])
+	]]
+
+	print(string.find("<vertices>3", "<vertices>"))
+
+	print("===============================================")
 end
 
 
 
 -- main
-local test_list={"test", "drive", "names"}
+local test_list={"Werra-Meissner-Kreis"
+		 ,"Kassel-City"
+		 ,"Kassel"
+		 ,"Waldeck-Frankenberg"
+		 ,"Hersfeld-Rotenburg"
+		 ,"Schwalm-Eder-Kreis"
+		 ,"Fulda"
+		 ,"Marburg-Biedenkopf"
+		 ,"Lahn-Dill-Kreis"
+		 ,"Limburg-Weilburg"
+		 ,"Giessen"
+		 ,"Vogelsbergkreis"
+		 ,"Main-Kinzig-Kreis"
+		 ,"Wetteraukreis"
+		 ,"Rheingau-Taunus-Kreis"
+		 ,"Hochtaunuskreis"
+		 ,"Wiesbaden"
+		 ,"Main-Taunus-Kreis"
+		 ,"Frankfurt-am-Main"
+		 ,"Offenbach-am-Main"
+		 ,"Offenbach"
+		 ,"Gross-Gerau"
+		 ,"Darmstadt"
+		 ,"Darmstadt-Dieburg"
+		 ,"Odenwaldkreis"
+		 ,"Bergstrasse"}
 local path = "../geometry"
 local file = "DE-HE"
 local ftype = "ugx"
@@ -141,7 +257,5 @@ local comment = "#"
 parse_data(path, file, ftype, comment, test_list)
 
 
-print("===============================================")
-print("test results:")
 --testing()
 
