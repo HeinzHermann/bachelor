34705a5123e955be3aa31c3ba8beb342ebfcbc98
HeinzHermann <marvin.glaser91@gmail.com>
2021-11-11 11:54:46 +0100

    added data and code to sensitivity analysis

--- data/python/sensitivity_plot.py
+++ data/python/sensitivity_plot.py
@@ -3,13 +3,14 @@ from mpl_toolkits import mplot3d
 import matplotlib as mpl
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 #%matplotlib inline
 
 # ========================================================================
 # part one, parse sample data
 
 # open and read sample file
-impFile = open("./samples10k.txt", "r")
+impFile = open("./samples4k.txt", "r")
 data = impFile.read()
 
 # set up for data parsing
@@ -55,34 +56,75 @@ for set_i in range(len(results)):
 # close file
 impFile.close()
 
+print("alpha: min =", min(res_sorted[0]), ", max =", max(res_sorted[0]))
+print("qq: min =", min(res_sorted[1]), ", max =", max(res_sorted[1]))
+print("loss: min =", min(res_sorted[2]), ", max =", max(res_sorted[2]))
+
+
+# set ranges for data point removal
+alpha_high = 0.25
+alpha_low = 0.1
+qq_high = 8
+qq_low = 5.5
+loss_high = 2700000000.000
+loss_low = 0
+
+# remove data points outsite of ranges
+data_count = len(res_sorted[2])
+for pos_i in range(1, data_count+1):
+	if (res_sorted[0][data_count - pos_i] > alpha_high or res_sorted[0][data_count - pos_i] < alpha_low
+			or res_sorted[1][data_count - pos_i] > qq_high or res_sorted[1][data_count - pos_i] < qq_low
+			or res_sorted[2][data_count - pos_i] > loss_high or res_sorted[2][data_count - pos_i] < loss_low):
+
+		#print (res_sorted[2][data_count - pos_i]) 
+		del res_sorted[0][data_count - pos_i] 
+		del res_sorted[1][data_count - pos_i] 
+		del res_sorted[2][data_count - pos_i] 
+
+print("alpha count =", len(res_sorted[0]))
+print("qq count =", len(res_sorted[1]))
+print("loss count =", len(res_sorted[2]))
+
+print("alpha: min =", min(res_sorted[0]), ", max =", max(res_sorted[0]))
+print("qq: min =", min(res_sorted[1]), ", max =", max(res_sorted[1]))
+print("loss: min =", min(res_sorted[2]), ", max =", max(res_sorted[2]))
+
 
 # ========================================================================
 # part two, plot results
 
-res_cut = [res_sorted[0][0:10],res_sorted[1][0:10],res_sorted[2][0:10]]
-
-"""
-test = [1,3,4,6]
-
-test2 = np.array(test)
-print(test)
-print(test2)
-test2.reshape(4, 4)
-print(test2)
-"""
 # some code for plotting
-alpha, qq = np.meshgrid(res_sorted[0], res_sorted[1])
-
 fig=plt.figure()
-ax=plt.axes(projection='3d')
-#ax.contour3D(alpha, qq, res_sorted[2], 50, cmap='binary')
-#ax.plot3D(res_sorted[0], res_sorted[1], res_sorted[2])
-ax.scatter3D(res_sorted[0], res_sorted[1], res_sorted[2])
-#ax.plot3D(res_cut[0], res_cut[1], res_cut[2])
-#ax.scatter3D(res_cut[0], res_cut[1], res_cut[2])
 
-ax.set_xlabel('alpha')
-ax.set_ylabel('qq')
-ax.set_zlabel('loss')
+# set plot type
+plot = 'scatter'
+#plot = 'triang'
+
+
+if plot == 'scatter':
+	# scatter plot
+	m_loss = max(res_sorted[2])
+	cm = plt.get_cmap("RdYlGn")
+	color = [cm(1-(loss/m_loss)) for loss in res_sorted[2]]
+
+	ax=plt.axes(projection='3d')
+	ax.scatter3D(res_sorted[0], res_sorted[1], res_sorted[2], c = color, marker = 'o')
+
+	ax.set_xlabel('alpha')
+	ax.set_ylabel('qq')
+	ax.set_zlabel('loss')
+
+	#plt.ticklabel_format(axis="z", style="sci", scilimits=(0,0))
+
+	ax.set_xlim3d(alpha_low, alpha_high)
+	ax.set_ylim3d(qq_low, qq_high)
+	ax.set_zlim3d(min(res_sorted[2]), max(res_sorted[2]))
+
+elif plot == 'triang':
+	# Triangulation
+	ax = Axes3D(fig)
+	ax.plot_trisurf(res_sorted[0], res_sorted[1], res_sorted[2])
+	#surf = ax.plot_trisurf(res_sorted[0], res_sorted[1], res_sorted[2], cmap = mpl.cm.jet, linewidth=0.1)
 
 plt.show()
+
