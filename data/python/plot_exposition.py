f64d3def98f4c23eed4a72cc5d110a5f0de3a3bd
HeinzHermann <marvin.glaser91@gmail.com>
2022-01-23 15:55:55 +0100

    new_testing with pso

--- data/python/plot_exposition.py
+++ data/python/plot_exposition.py
@@ -10,7 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 # part one, parse sample data
 
 # set up for data parsing
-file_names = ["./t50_run_orig.txt","t50_run_sim.txt"]
+file_names = ["./t75_run_orig.txt","./s_pso_all.txt"]
 results = []
 sorted_tab = []
 string = ''
@@ -82,7 +82,7 @@ fit = []
 functions = []
 extrapolation = []
 
-extra_start = 50
+extra_start = 75
 extra_end = 75
 extrapolation.append(range(extra_start, extra_end+1))
 
@@ -127,40 +127,27 @@ if plot == 'scatter':
 	ax = fig.add_subplot(111)
 
 	set_start = 1
-	#set_end = 5
-	set_end = len(sorted_tab)
+	set_end = 27
+	#set_end = len(sorted_tab)
+	set_end = set_start+1 # only show one graph
 
-	for set_i in range(set_start, set_end):
-		ax.scatter(sorted_tab[0], sorted_tab[set_i], label=results[0][set_i])
+	for file_i in range(len(results)):
+		for set_i in range(set_start, set_end):
+			ax.scatter(sorted_tab[file_i][0], sorted_tab[file_i][set_i], label=results[file_i][0][set_i])
 
-	plt.legend(loc='upper right')
+	#for set_i in range(set_start, set_end):
+		#ax.scatter(extrapolation[0], extrapolation[set_i], label=results[1][0][set_i][:-3]+"extrapolation")
 
-	"""
-	# scatter plot
-	m_loss = max(res_sorted[2])
-	cm = plt.get_cmap("RdYlGn")
-	color = [cm(1-(loss/m_loss)) for loss in res_sorted[2]]
-
-	ax=plt.axes(projection='3d')
-	ax.scatter3D(res_sorted[0], res_sorted[1], res_sorted[2], c = color, marker = 'o')
-
-	ax.set_xlabel('alpha')
-	ax.set_ylabel('qq')
-	ax.set_zlabel('loss')
-
-	#plt.ticklabel_format(axis="z", style="sci", scilimits=(0,0))
-
-	ax.set_xlim3d(alpha_low, alpha_high)
-	ax.set_ylim3d(qq_low, qq_high)
-	ax.set_zlim3d(min(res_sorted[2]), max(res_sorted[2]))
-	"""
-
+	plt.axvline(x=50, color='r', linestyle=':')
+	plt.legend(loc='upper left')
+	plt.xlabel("Time t (days)")
+	plt.ylabel("Sum of Exposed")
 	plt.show()
 
 elif plot == 'plot':
 	ax = fig.add_subplot(111)
 
-	set_start = 13
+	set_start = 17
 	set_end = 27
 	#set_end = len(sorted_tab)
 	set_end = set_start+1 # only show one graph
@@ -169,10 +156,10 @@ elif plot == 'plot':
 		for set_i in range(set_start, set_end):
 			ax.plot(sorted_tab[file_i][0], sorted_tab[file_i][set_i], label=results[file_i][0][set_i])
 
-	for set_i in range(set_start, set_end):
-		ax.plot(extrapolation[0], extrapolation[set_i], label=results[1][0][set_i][:-3]+"extrapolation")
+	#for set_i in range(set_start, set_end):
+	#	ax.plot(extrapolation[0], extrapolation[set_i], label=results[1][0][set_i][:-3]+"extrapolation")
 
-	plt.axvline(x=50, color='r', linestyle=':')
+	#plt.axvline(x=60, color='r', linestyle=':')
 	plt.legend(loc='upper left')
 	plt.xlabel("Time t (days)")
 	plt.ylabel("Sum of Exposed")
